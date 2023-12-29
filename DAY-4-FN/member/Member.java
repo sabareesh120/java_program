3f0c2d4e29fb33732dff74b9663365a74c9cb380
S.Sabareesh <111498473+sabareesh120@users.noreply.github.com>
2023-12-29 12:12:39 +0530

    Create Member.java

---
+++ DAY-4-FN/member/Member.java
@@ -0,0 +1,11 @@
+class Member {
+    String name;
+    int age;
+    String phoneNumber;
+    String address;
+    double salary;
+
+    public void printSalary() {
+        System.out.println("Salary: Rs." + salary);
+    }
+}
