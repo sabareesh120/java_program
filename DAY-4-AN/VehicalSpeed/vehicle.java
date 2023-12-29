d18c974ae049184764f8c6c71543bccfaf71d37d
S.Sabareesh <111498473+sabareesh120@users.noreply.github.com>
2023-12-29 17:30:42 +0530

    Create vehicle.java

---
+++ DAY-4-AN/VehicalSpeed/vehicle.java
@@ -0,0 +1,11 @@
+class Vehicle {
+    private double speed;
+
+    public Vehicle(double speed) {
+        this.speed = speed;
+    }
+
+    public double calculateSpeed() {
+        return speed;
+    }
+}
